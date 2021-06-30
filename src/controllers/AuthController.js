e4afbfc9a7d8573fee119fd1106a1ce68b74ec41
rodrigogeraldes <rodrigo.geraldes@aluno.ifsp.edu.br>
2021-06-30 10:33:28 -0300

    AuthController update

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -4,14 +4,6 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   async login(req, res) {
     const {usr_email, usr_senha} = req.body;
-    
-    if (!usr_email) {
-      return res.status(400).send({error: "Campo Email deve ser preenchido!"});
-    }
-
-    if (!usr_senha) {
-      return res.status(400).send({error: "Campo Senha deve ser preenchido!"});
-    }
 
     const usuario = await Usuario.findOne({where: {usr_email, usr_senha}});
 
