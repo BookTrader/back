bcbf5663a5b309265da0707b35bb46bbb2068852
wesferoli <wesleyfer.oli97@gmail.com>
2021-11-20 19:17:06 -0300

    Arrumando rotas dos imports

---
+++ src/app/model/Anuncio.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Anuncio extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        anc_descricao: DataTypes.STRING,
+      }, {
+        sequelize, freezeTableName: true, modelName: 'anuncio'
+      }
+    )
+  }
+
+  static associate(models) {
+    this.belongsTo(models.usuario, { foreignKey: 'usr_id', as: 'usuario' })
+    this.belongsTo(models.exemplar, { foreignKey: 'exm_id', as: 'exemplar' })
+  }
+}
+
+module.exports = Anuncio;
---
+++ src/app/model/Usuario.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Usuario extends Model {
+  static init(sequelize) {
+    super.init({
+      usr_apelido: DataTypes.STRING,
+      usr_nome: DataTypes.STRING,
+      usr_foto: DataTypes.STRING,
+      usr_cpf: DataTypes.STRING,
+      usr_ender_uf: DataTypes.STRING,
+      usr_ender_cep: DataTypes.INTEGER,
+      usr_ender_cidade: DataTypes.STRING,
+      usr_ender_bairro: DataTypes.STRING,
+      usr_email: DataTypes.STRING,
+      usr_senha: DataTypes.STRING,
+      usr_range_troca: DataTypes.FLOAT,
+      usr_latitude: DataTypes.DECIMAL(10, 8),
+      usr_longitude: DataTypes.DECIMAL(10, 8),
+      reset_token: DataTypes.STRING,
+      reset_token_expires: DataTypes.DATE
+    }, {
+      sequelize, freezeTableName: true, modelName: 'usuario'
+    })
+  }
+
+  static associate(models) {
+    this.hasMany(models.anuncio, { foreignKey: 'usr_id', as: 'anuncios' })
+    this.hasMany(models.exemplar, { foreignKey: 'usr_id', as: 'exemplares' })
+  }
+}
+
+module.exports = Usuario;
